a00504302c7cdfcee697342e779481b44c2d870c
Felipe <fen1499@gmail.com>
2021-03-14 16:18:53 -0300

    wip

--- report_generator/lib/report_generator.ex
+++ report_generator/lib/report_generator.ex
@@ -1,5 +1,12 @@
 defmodule ReportGenerator do
-  def hello do
-    :world
+
+  alias ReportGenerator.Parser
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), &sum_values/2)
   end
+
+  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end

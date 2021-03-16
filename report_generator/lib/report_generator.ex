cab7cd3530476bb1f8a4d8574e610040c79fe685
Felipe <fen1499@gmail.com>
2021-03-15 23:40:47 -0300

    wip

--- report_generator/lib/report_generator.ex
+++ report_generator/lib/report_generator.ex
@@ -1,6 +1,17 @@
 defmodule ReportGenerator do
-
   alias ReportGenerator.Parser
+
+  @avalible_foods [
+    "açaí",
+    "churrasco",
+    "hambúrguer",
+    "esfirra",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
@@ -9,6 +20,19 @@ defmodule ReportGenerator do
 
   def fetch_higher_cost(report), do: Enum.max_by(report, fn {_k, v} -> v end)
 
-  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
-  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  defp report_acc do
+    foods = Enum.into(@avalible_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    %{
+      "users" => users,
+      "foods" => foods
+    }
+  end
 end

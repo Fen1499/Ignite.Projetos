77ecf64420dcaf029a4e6b2a004266c4c73ed884
Felipe <fen1499@gmail.com>
2021-03-18 23:00:00 -0300

    async + tests

--- report_generator/lib/report_generator.ex
+++ report_generator/lib/report_generator.ex
@@ -23,6 +23,24 @@ defmodule ReportGenerator do
     |> Enum.reduce(report_acc(), &sum_values/2)
   end
 
+  def build_from_many(filenames) when not is_list(filenames), do: {:error, "please provide a list of strings!"}
+  def build_from_many(filenames) do
+    ret = filenames
+    |> Task.async_stream(&build(&1))
+    |> Enum.reduce(report_acc(), fn {:ok, res}, report -> sum_reports(report, res) end)
+    {:ok, ret}
+  end
+
+  def sum_reports(%{"foods" => foods1, "users" => users1}, %{"foods" => foods2, "users" => users2}) do
+    foods = Map.merge(foods1, foods2, fn _key, value1, value2 -> value1 + value2 end)
+    users = Map.merge(users1, users2, fn _key, value1, value2 -> value1 + value2 end)
+
+    %{
+     "foods" => foods,
+     "users" => users
+    }
+  end
+
   def fetch_higher_cost(report, option) when option in @options do
     {:ok, Enum.max_by(report[option], fn {_k, v} -> v end)}
   end
--- report_generator/test/report_generator_test.exs
+++ report_generator/test/report_generator_test.exs
@@ -12,6 +12,25 @@ defmodule ReportGeneratorTest do
     end
   end
 
+  describe "build_from_many\1" do
+    test "when a file list is provided builds the report" do
+      file_name = "report_test.csv"
+
+      response = ReportGenerator.build_from_many([file_name, file_name])
+      expected_response =  {:ok, %{"foods" => %{"a├ºa├¡" => 1, "churrasco" => 2, "esfirra" => 3, "hamb├║rguer" => 2, "pastel" => 0, "pizza" => 2, "prato_feito" => 0, "sushi" => 0}, "users" => %{"1" => 48, "10" => 36, "11" => 0, "12" => 0, "13" => 0, "14" => 0, "15" => 0, "16" => 0, "17" => 0, "18" => 0, "19" => 0, "2" => 45, "20" => 0, "21" => 0, "22" => 0, "23" => 0, "24" => 0, "25" => 0, "26" => 0, "27" => 0, "28"
+        => 0, "29" => 0, "3" => 31, "30" => 0, "4" => 42, "5" => 49, "6" => 18, "7" => 27, "8" => 25, "9" => 24}}}
+
+      assert response == expected_response
+    end
+
+    test "when a file build is provided returns an error" do
+      response = ReportGenerator.build_from_many("eh")
+      expected_response = {:error, "please provide a list of strings!"}
+
+      assert response == expected_response
+    end
+  end
+
   describe "fetch_higher_cost\2" do
 
     test "when the option is 'users', returns the user who spent the most" do

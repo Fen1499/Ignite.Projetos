d67378be447e6a3d61b42d531215c8c8f440a6f9
Felipe <fen1499@gmail.com>
2021-03-21 22:40:37 -0300

    test agents

--- exlivery/lib/exlivery.ex
+++ exlivery/lib/exlivery.ex
@@ -1,5 +1,14 @@
 defmodule Exlivery do
-  def hello do
-    :world
+  alias Exlivery.Users.CreateOrUpdate, as: CreateOrUpdateUsers
+  alias Exlivery.Orders.CreateOrUpdate, as: CreateOrUpdateOrders
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Orders.Agent, as: OrderAgent
+
+  def start_agents do
+    UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUsers, as: :call
+  defdelegate create_of_update_order(params), to: CreateOrUpdateOrders, as: :call
 end
---
+++ exlivery/lib/users/create_or_update.ex
@@ -0,0 +1,12 @@
+defmodule Exlivery.Users.CreateOrUpdate do
+  alias Exlivery.Users.User
+  alias Exlivery.Users.Agent, as: UserAgent
+
+  def call(%{name: name, address: address, email: email, cpf: cpf, age: age}) do
+    User.build(name, email, cpf, age, address)
+    |> save_user()
+  end
+
+  defp save_user({:ok, %User{} = user}), do: UserAgent.save(user)
+  defp save_user({:error, _reason} = error), do: error
+end

8ade4d09e14bfbb8b9022a281905c6b54def4298
Felipe <fen1499@gmail.com>
2021-04-04 01:06:30 -0300

    second commit

--- rockelivery/config/dev.exs
+++ rockelivery/config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :rockelivery, Rockelivery.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "felipe123",
   database: "rockelivery_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
